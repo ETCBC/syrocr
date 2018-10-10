e602aed4271d3f31a11b9903be4dd0bbe48f0346
Hannes Vlaardingerbroek <hannes@vlaardingerbroek.nl>
2018-10-10 12:08:17 +0300

    Parse arguments with argparse

--- syrocr.py
+++ syrocr.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
+from syrocr.getlines import getlines, drawboxes
+import os.path, json
+
 def usage():
     print("""Usage:
 python3 syrocr.py getlines filename""")
 
 if __name__ == "__main__":
-    # TODO This should be done with argparse
-    import sys
-    args = sys.argv[1:]
-    if len(args) < 2:
-        usage()
-        sys.exit(2)
-    if args[0] == "getlines":
-        # try:
-        from syrocr.getlines import getlines, drawboxes
-        import os.path, json
-        filepath = args[1]
-        basename = os.path.basename(os.path.splitext(filepath)[0])
-        lines = getlines(filepath, dpi=(300,300), verbose=False)
-        im_lines = drawboxes(filepath, lines)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose",
+        help="increase output verbosity",
+        action="store_true")
+    parser.add_argument("--getlines",
+        metavar='source_image',
+        required=True,
+        help="""Get lines from source_image,
+                write to json file, and save image with drawboxes""")
+
+    args = parser.parse_args()
+
+    if args.getlines:
+        source_file = args.getlines
+        basename = os.path.basename(os.path.splitext(source_file)[0])
+        lines = getlines(source_file, dpi=(300,300), verbose=False)
+        im_lines = drawboxes(source_file, lines)
         im_lines.save(basename + '_lines.png', format="PNG")
         with open(basename + '_lines.json', 'w') as f:
             json.dump(lines, f, indent=2)
