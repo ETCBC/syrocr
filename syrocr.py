79d2620bb8b54daddb76aa8c84f0499cf6aa7fcd
Hannes Vlaardingerbroek <hannes@vlaardingerbroek.nl>
2018-10-10 12:32:54 +0300

    Add --drawboxes to argparse options

--- syrocr.py
+++ syrocr.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
-from syrocr.getlines import getlines, drawboxes
-import os.path, json
-
-def usage():
-    print("""Usage:
-python3 syrocr.py getlines filename""")
-
 if __name__ == "__main__":
-    import argparse
+    import argparse, json, os.path
+    from syrocr.getlines import getlines, drawboxes
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose",
         help="increase output verbosity",
         action="store_true")
-    parser.add_argument("--getlines",
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--getlines",
         metavar='source_image',
-        required=True,
         help="""Get lines from source_image,
                 write to json file, and save image with drawboxes""")
+    group.add_argument("--drawboxes",
+        nargs=2,
+        metavar=('source_image', 'json_file'),
+        help="Draw boxes from lines in json_file on source_image")
 
     args = parser.parse_args()
 
     if args.getlines:
-        source_file = args.getlines
-        basename = os.path.basename(os.path.splitext(source_file)[0])
-        lines = getlines(source_file, dpi=(300,300), verbose=False)
-        im_lines = drawboxes(source_file, lines)
+        source_image = args.getlines
+        basename = os.path.basename(os.path.splitext(source_image)[0])
+        lines = getlines(source_image, dpi=(300,300), verbose=False)
+        im_lines = drawboxes(source_image, lines)
         im_lines.save(basename + '_lines.png', format="PNG")
         with open(basename + '_lines.json', 'w') as f:
             json.dump(lines, f, indent=2)
+    elif args.drawboxes:
+        source_image, json_file = args.drawboxes
+        basename = os.path.basename(os.path.splitext(source_image)[0])
+        with open(json_file) as f:
+            lines = json.load(f)
+        im_lines = drawboxes(source_image, lines)
+        im_lines.save(basename + '_lines.png', format="PNG")
